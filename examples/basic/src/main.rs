45272a978e52af00553e970b811807762aa1f4f8
Alisue <lambdalisue@gmail.com>
2022-08-21 00:57:17 +0900

    Update example to use fibonacci

--- examples/basic/src/main.rs
+++ examples/basic/src/main.rs
@@ -1,5 +1,6 @@
+use rand::{thread_rng, Rng};
 use std::thread;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 use metrics_exporter_prometheus::PrometheusBuilder;
 use metrics_process::Collector;
@@ -13,8 +14,26 @@ fn main() {
     let collector = Collector::new("");
     collector.describe();
 
+    let mut rng = thread_rng();
+
     loop {
+        let s = Instant::now();
+        let n: u64 = rng.gen_range(0..40);
+        println!(
+            "fibonacci({}) = {} ({} ns)",
+            n,
+            fibonacci(n),
+            s.elapsed().as_nanos()
+        );
         collector.collect();
         thread::sleep(Duration::from_millis(750));
+    }
+}
+
+fn fibonacci(n: u64) -> u64 {
+    match n {
+        0 => 0,
+        1 => 1,
+        _ => fibonacci(n - 2) + fibonacci(n - 1),
     }
 }
