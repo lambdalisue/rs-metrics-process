9214f8200e627ef9681c24a9a578aee18d39b385
Alisue <lambdalisue@gmail.com>
2022-08-23 04:30:31 +0900

    Add example with axum

---
+++ examples/with_axum/README.md
@@ -0,0 +1,16 @@
+# with_axum
+
+An example of `metrics-process` crate with [axum][] and [metrics-exporter-prometheus][].
+
+[axum]: https://crates.io/crates/axum
+[metrics-exporter-prometheus]: https://crates.io/crates/metrics-exporter-prometheus
+
+## Usage
+
+Start the server with the following command
+
+```
+cargo run --release
+```
+
+Then access to http://localhost:9000/metrics
---
+++ examples/with_axum/src/main.rs
@@ -0,0 +1,29 @@
+use axum::{routing::get, Router, Server};
+use metrics_exporter_prometheus::PrometheusBuilder;
+use metrics_process::Collector;
+
+#[tokio::main]
+async fn main() {
+    let builder = PrometheusBuilder::new();
+    let handle = builder
+        .install_recorder()
+        .expect("failed to install Prometheus recorder");
+
+    let collector = Collector::new("");
+    // Call `describe()` method to register help string.
+    collector.describe();
+
+    let addr = "127.0.0.1:9000".parse().unwrap();
+    let app = Router::new().route(
+        "/metrics",
+        get(move || {
+            // Collect information just before handle '/metrics'
+            collector.collect();
+            std::future::ready(handle.render())
+        }),
+    );
+    Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
