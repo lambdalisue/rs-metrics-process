9fa56ae139b0c22fc75e8482c4c82e3043367f87
Alisue <lambdalisue@gmail.com>
2022-08-23 06:33:59 +0900

    Fix `cpu_seconds_total` in macOS (Apple Sillicon)
    It seems `ri_user_time/ri_system_time` is NOT nanoseconds.
    It's nanoseconds in intel mac because info.numer/info.denom = 1/1
    but in Apple Sillicon (125/3).

--- src/collector/macos.rs
+++ src/collector/macos.rs
@@ -2,19 +2,34 @@ use libproc::libproc::file_info::ListFDs;
 use libproc::libproc::pid_rusage::{pidrusage, RUsageInfoV2};
 use libproc::libproc::proc_pid::{listpidinfo, pidinfo};
 use libproc::libproc::task_info::TaskAllInfo;
+use mach2::mach_time;
+use once_cell::sync::Lazy;
 use rlimit::{getrlimit, Resource};
+use std::mem::MaybeUninit;
 use std::process;
 
 use super::Metrics;
+
+// https://stackoverflow.com/a/72915413
+// https://openradar.appspot.com/FB9546856
+// https://developer.apple.com/documentation/kernel/mach_timebase_info_data_t
+static TIMEBASE_TO_NANOSECONDS: Lazy<f64> = Lazy::new(|| {
+    let mut info = MaybeUninit::uninit();
+    let info = unsafe {
+        mach_time::mach_timebase_info(info.as_mut_ptr());
+        info.assume_init()
+    };
+    info.numer as f64 / info.denom as f64
+});
 
 pub fn collect() -> Metrics {
     let pid = process::id() as i32;
     let mut metrics = Metrics::default();
     if let Ok(res) = pidrusage::<RUsageInfoV2>(pid) {
         metrics.cpu_seconds_total = {
-            // Unit of 'ri_xxxx_time' is 'nano' (10^-9) seconds
             let t = res.ri_user_time + res.ri_system_time;
-            Some((t as f64) * 1e-9)
+            let t = t as f64 * *TIMEBASE_TO_NANOSECONDS / 1e9;
+            Some(t)
         };
     }
     if let Ok(info) = pidinfo::<TaskAllInfo>(pid, 0) {
