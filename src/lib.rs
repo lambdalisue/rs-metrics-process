40ca9edcd92c0fefc5107c2910fe63539600c609
zohnannor <zohnannor@gmail.com>
2023-12-28 12:16:07 +0400

    Review corrections

--- src/lib.rs
+++ src/lib.rs
@@ -97,7 +97,7 @@
 
 mod collector;
 
-use std::{mem, sync::Arc};
+use std::sync::Arc;
 
 use metrics::{describe_gauge, gauge, Unit};
 
@@ -116,9 +116,9 @@ struct Metrics {
 
 impl Metrics {
     // Create new Metrics, allocating prefixed strings for metrics names.
-    fn new(prefix: impl AsRef<str>) -> Arc<Self> {
+    fn new(prefix: impl AsRef<str>) -> Self {
         let prefix = prefix.as_ref();
-        Arc::new(Self {
+        Self {
             cpu_seconds_total: format!("{prefix}process_cpu_seconds_total").into(),
             open_fds: format!("{prefix}process_open_fds").into(),
             max_fds: format!("{prefix}process_max_fds").into(),
@@ -127,23 +127,14 @@ impl Metrics {
             resident_memory_bytes: format!("{prefix}process_resident_memory_bytes").into(),
             start_time_seconds: format!("{prefix}process_start_time_seconds").into(),
             threads: format!("{prefix}process_threads").into(),
-        })
+        }
     }
 }
 
 impl Default for Metrics {
     // Create new Metrics, without prefixing and thus allocating.
     fn default() -> Self {
-        Self {
-            cpu_seconds_total: "process_cpu_seconds_total".into(),
-            open_fds: "process_open_fds".into(),
-            max_fds: "process_max_fds".into(),
-            virtual_memory_bytes: "process_virtual_memory_bytes".into(),
-            virtual_memory_max_bytes: "process_virtual_memory_max_bytes".into(),
-            resident_memory_bytes: "process_resident_memory_bytes".into(),
-            start_time_seconds: "process_start_time_seconds".into(),
-            threads: "process_threads".into(),
-        }
+        Self::new("")
     }
 }
 
@@ -154,7 +145,8 @@ pub struct Collector {
 }
 
 impl Default for Collector {
-    /// Create a new Collector instance without prefix.
+    /// Create a new Collector instance without prefix. This is the same as
+    /// calling `Collector::new` with an empty string for prefix.
     ///
     /// # Examples
     ///
@@ -177,6 +169,16 @@ impl Collector {
     /// # use metrics_process::Collector;
     /// let collector = Collector::default().prefix("my_prefix_");
     /// ```
+    ///
+    /// # Deprecated
+    ///
+    /// The new interface for creating a Collector should be utilized.
+    ///
+    /// ```
+    /// # use metrics_process::Collector;
+    /// let collector = Collector::new("my_prefix_");
+    /// ```
+    #[deprecated(since = "1.1.0", note = "Use `Collector::new(prefix)`.")]
     pub fn prefix(self, prefix: impl Into<String>) -> Self {
         drop(self);
         Self::new(prefix.into())
@@ -193,7 +195,7 @@ impl Collector {
     /// ```
     pub fn new(prefix: impl AsRef<str>) -> Self {
         Self {
-            metrics: Metrics::new(prefix),
+            metrics: Arc::new(Metrics::new(prefix)),
         }
     }
 
@@ -219,44 +221,44 @@ impl Collector {
         let metrics = self.metrics.as_ref();
 
         describe_gauge!(
-            metrics.cpu_seconds_total.clone(),
+            Arc::clone(&metrics.cpu_seconds_total),
             Unit::Seconds,
             "Total user and system CPU time spent in seconds."
         );
         describe_gauge!(
-            metrics.open_fds.clone(),
+            Arc::clone(&metrics.open_fds),
             Unit::Count,
             "Number of open file descriptors."
         );
         describe_gauge!(
-            metrics.max_fds.clone(),
+            Arc::clone(&metrics.max_fds),
             Unit::Count,
             "Maximum number of open file descriptors."
         );
         describe_gauge!(
-            metrics.virtual_memory_bytes.clone(),
+            Arc::clone(&metrics.virtual_memory_bytes),
             Unit::Bytes,
             "Virtual memory size in bytes."
         );
         #[cfg(not(target_os = "windows"))]
         describe_gauge!(
-            metrics.virtual_memory_max_bytes.clone(),
+            Arc::clone(&metrics.virtual_memory_max_bytes),
             Unit::Bytes,
             "Maximum amount of virtual memory available in bytes."
         );
         describe_gauge!(
-            metrics.resident_memory_bytes.clone(),
+            Arc::clone(&metrics.resident_memory_bytes),
             Unit::Bytes,
             "Resident memory size in bytes."
         );
         describe_gauge!(
-            metrics.start_time_seconds.clone(),
+            Arc::clone(&metrics.start_time_seconds),
             Unit::Seconds,
             "Start time of the process since unix epoch in seconds."
         );
         #[cfg(not(target_os = "windows"))]
         describe_gauge!(
-            metrics.threads.clone(),
+            Arc::clone(&metrics.threads),
             Unit::Count,
             "Number of OS threads in the process."
         );
@@ -285,30 +287,30 @@ impl Collector {
         let metrics = self.metrics.as_ref();
         let mut m = collector::collect();
         if let Some(v) = m.cpu_seconds_total.take() {
-            gauge!(metrics.cpu_seconds_total.clone()).set(v);
+            gauge!(Arc::clone(&metrics.cpu_seconds_total)).set(v);
         }
         if let Some(v) = m.open_fds.take() {
-            gauge!(metrics.open_fds.clone()).set(v as f64);
+            gauge!(Arc::clone(&metrics.open_fds)).set(v as f64);
         }
         if let Some(v) = m.max_fds.take() {
-            gauge!(metrics.max_fds.clone()).set(v as f64);
+            gauge!(Arc::clone(&metrics.max_fds)).set(v as f64);
         }
         if let Some(v) = m.virtual_memory_bytes.take() {
-            gauge!(metrics.virtual_memory_bytes.clone()).set(v as f64);
+            gauge!(Arc::clone(&metrics.virtual_memory_bytes)).set(v as f64);
         }
         #[cfg(not(target_os = "windows"))]
         if let Some(v) = m.virtual_memory_max_bytes.take() {
-            gauge!(metrics.virtual_memory_max_bytes.clone()).set(v as f64);
+            gauge!(Arc::clone(&metrics.virtual_memory_max_bytes)).set(v as f64);
         }
         if let Some(v) = m.resident_memory_bytes.take() {
-            gauge!(metrics.resident_memory_bytes.clone()).set(v as f64);
+            gauge!(Arc::clone(&metrics.resident_memory_bytes)).set(v as f64);
         }
         if let Some(v) = m.start_time_seconds.take() {
-            gauge!(metrics.start_time_seconds.clone()).set(v as f64);
+            gauge!(Arc::clone(&metrics.start_time_seconds)).set(v as f64);
         }
         #[cfg(not(target_os = "windows"))]
         if let Some(v) = m.threads.take() {
-            gauge!(metrics.threads.clone()).set(v as f64);
+            gauge!(Arc::clone(&metrics.threads)).set(v as f64);
         }
     }
 }
