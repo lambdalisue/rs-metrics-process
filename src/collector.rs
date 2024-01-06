2bbabfdc2c7499d4ad4d11ee71a30f719540b8c6
Alisue <lambdalisue@gmail.com>
2024-01-06 15:41:52 +0900

    Add `dummy` feature for non supported platforms
    Based on the discussion in #36, we decided to add a `dummy` feature
    that always return an empty `Metrics` for non supported platforms.

--- src/collector.rs
+++ src/collector.rs
@@ -1,12 +1,19 @@
-#[cfg(target_os = "macos")]
-mod macos;
+#[cfg_attr(target_os = "macos", path = "collector/macos.rs")]
+#[cfg_attr(target_os = "linux", path = "collector/linux.rs")]
+#[cfg_attr(target_os = "windows", path = "collector/windows.rs")]
+#[allow(unused_attributes)]
+#[cfg_attr(feature = "dummy", path = "collector/dummy.rs")]
+mod collector;
 
-#[cfg(target_os = "linux")]
-mod linux;
+#[cfg(all(
+    not(feature = "dummy"),
+    not(any(target_os = "macos", target_os = "linux", target_os = "windows"))
+))]
+compile_error!(
+    "A feature \"dummy\" must be enabled to compile this crate on non supported platforms."
+);
 
-#[cfg(target_os = "windows")]
-#[path = "collector/windows.rs"]
-mod win;
+pub use collector::collect;
 
 /// Process metrics
 /// https://prometheus.io/docs/instrumenting/writing_clientlibs/#process-metrics
@@ -32,15 +39,6 @@ pub struct Metrics {
     pub threads: Option<u64>,
 }
 
-#[cfg(target_os = "macos")]
-pub use macos::collect;
-
-#[cfg(target_os = "linux")]
-pub use linux::collect;
-
-#[cfg(target_os = "windows")]
-pub use win::collect;
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -54,6 +52,7 @@ mod tests {
         }
     }
 
+    #[cfg(any(target_os = "macos", target_os = "linux", target_os = "windows"))]
     #[test]
     fn test_collect_internal_ok() {
         fibonacci(40);
@@ -69,5 +68,24 @@ mod tests {
         assert_matches!(m.start_time_seconds, Some(_));
         #[cfg(not(target_os = "windows"))]
         assert_matches!(m.threads, Some(_));
+    }
+
+    #[cfg(not(target_os = "macos"))]
+    #[cfg(not(target_os = "linux"))]
+    #[cfg(not(target_os = "windows"))]
+    #[cfg(feature = "dummy")]
+    #[test]
+    fn test_collect_internal_ok_dummy() {
+        fibonacci(40);
+        let m = collect();
+        dbg!(&m);
+        assert_matches!(m.cpu_seconds_total, None);
+        assert_matches!(m.open_fds, None);
+        assert_matches!(m.max_fds, None);
+        assert_matches!(m.virtual_memory_bytes, None);
+        assert_matches!(m.virtual_memory_max_bytes, None);
+        assert_matches!(m.resident_memory_bytes, None);
+        assert_matches!(m.start_time_seconds, None);
+        assert_matches!(m.threads, None);
     }
 }
---
+++ src/collector/dummy.rs
@@ -0,0 +1,5 @@
+use super::Metrics;
+
+pub fn collect() -> Metrics {
+    Metrics::default()
+}
